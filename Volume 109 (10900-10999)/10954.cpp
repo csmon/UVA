df32f13b79a52fa38c803133014033de1d356910
csmon <csmon7507@gmail.com>
2022-04-05 22:17:48 +0800

    Add uva10954
    submit timeout

---
+++ Volume 109 (10900-10999)/10954.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+#define TEST
+#ifdef TEST
+#include <gtest/gtest.h>
+#endif
+
+std::vector<int> save_data(const std::string &line) {
+    std::vector<int> inputs;
+    int number;
+    std::stringstream stream(line);
+    while (stream >> number) {
+        inputs.push_back(number);
+    }
+    return inputs;
+}
+
+int get_total_cost(std::vector<int> &inputs) {
+    int cost = 0;
+    int min_1, min_2;
+    while (inputs.empty() == false) {
+        std::sort(inputs.begin(), inputs.end(), std::greater<int>());
+        min_1 = inputs.back();
+        inputs.pop_back();
+        min_2 = inputs.back();
+        inputs.pop_back();
+        cost += (min_1 + min_2);
+        inputs.push_back(min_1 + min_2);
+        if (inputs.size() == 1) {
+            return cost;
+            break;
+        }
+    }
+    return 0;
+}
+
+void solve(std::istream & is, std::ostream & os) {
+   
+    while(true) {
+        std::string line;
+        std::getline(is, line);
+        int length = stoi(line);
+        if (length == 0)
+            break;
+
+        std::getline(is, line);
+        std::vector<int> inputs = save_data(line);
+
+        int cost = get_total_cost(inputs);
+
+        os << cost << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+#if defined(TEST)
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+#else
+    solve(std::cin, std::cout);
+#endif
+    return 0;
+}
+#ifdef TEST
+
+TEST(sample, uva10954)
+{
+    std::istringstream iss(R"(3
+1 2 3
+4
+1 2 3 4
+0
+)");
+    std::ostringstream oss;
+    solve(iss, oss);
+    EXPECT_EQ(R"(9
+19
+)", oss.str());
+}
+
+TEST(Shah_Shishir, uDebug)
+{
+    std::istringstream iss(R"(5
+5 4 3 1 2
+0
+)");
+    std::ostringstream oss;
+    solve(iss, oss);
+    EXPECT_EQ(R"(33
+)", oss.str());
+}
+
+TEST(caffeines, uDebug)
+{
+    std::istringstream iss(R"(3
+6 5 4
+10
+9 5 48 2 4 5 6 3 5 4
+10
+3 4 5 4 7 2 3 8 4 5
+0
+)");
+    std::ostringstream oss;
+    solve(iss, oss);
+    EXPECT_EQ(R"(24
+224
+147
+)", oss.str());
+}
+
+TEST(Eduardo_Figueiredo, uDebug)
+{
+    std::istringstream iss(R"(8
+4 4 6 6 8 8 10 10
+8
+4 6 6 7 7 8 8 9
+8
+3 4 6 6 6 8 8 9
+8
+3 4 5 5 5 5 8 9
+2
+99999 0
+4
+99997 49999 49999 49999
+4
+49999 99998 49999 49999
+0
+)");
+    std::ostringstream oss;
+    solve(iss, oss);
+    EXPECT_EQ(R"(166
+165
+148
+130
+99999
+499988
+499990
+)", oss.str());
+}
+#endif
